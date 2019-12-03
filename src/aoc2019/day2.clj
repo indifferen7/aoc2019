fcdf835eb6ef0bd03fa7c212668cce8a00917b65
Martin Moberg <martin@mobergarna.net>
2019-12-03 08:17:35 +0100

    day 2, part two

--- src/aoc2019/day2.clj
+++ src/aoc2019/day2.clj
@@ -8,7 +8,7 @@
 (defn multiply [state, idx1, idx2]
   (* (get state idx1) (get state idx2)))
 
-(defn part-one-algorithm [initial-state]
+(defn algorithm [initial-state]
   (loop [positions-left (range (count initial-state))
          state initial-state]
     (let [[opcode-pos a-pos b-pos target-pos] (take 4 positions-left)
@@ -24,17 +24,25 @@
             (drop 4 positions-left)
             (assoc-in state [target-idx] target-val)))))))
 
-(def initial-state-part-one
-  (-> (utils/read-resource "day2")
-      (string/split  #",")
-      (as-> parts
-            (->>
-              (map #(Integer/parseInt %) parts)
-              (map-indexed vector)
-              (into {})))
-      (assoc 1 12 2 2)))
+(def parsed-file (string/split (utils/read-resource "day2") #","))
+
+(defn initial-state [one two]
+  (let [state (->> (map #(Integer/parseInt %) parsed-file)
+                   (map-indexed vector)
+                   (into {}))]
+    (assoc state 1 one 2 two)))
 
 ; print solution for part 1
 (println "Solution part one:"
-         (get (part-one-algorithm initial-state-part-one) 0))
+         (get (algorithm (initial-state 12 2)) 0))
 
+(def part-two-solution
+  (for [one (range 99)
+        two (range 99)
+        :let [output
+              (get (algorithm (initial-state one two)) 0)]
+        :when (= 19690720 output)]
+      (+ two (* one 100 ))))
+
+; print solution for part 1
+(println "Solution part two:" part-two-solution)
